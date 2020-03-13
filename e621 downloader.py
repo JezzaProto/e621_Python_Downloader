e81fd7de3b6387e6cfd1de542a5917cb01e67111
JezzaR <jeremyruss43@gmail.com>
2020-03-13 21:24:37 +0000

    Update
    Made "all" rating option work...

--- e621 downloader.py
+++ e621 downloader.py
@@ -15,6 +15,8 @@ elif ratings.lower() == "-questionable":
     rating = "-rating:questionable"
 elif ratings.lower() == "-explicit":
     rating = "-rating:explicit"
+else:
+    rating = ""
 tag = str(input("Please enter what tags you would like (separated with a semicolon):\n"))
 tags = tag.split(";")
 postnum = str(input("Please enter how many posts you would like to download (Beware, a large number will take a while to download):\n"))
