ba389e1068a3080c5952ad5489a462a3ce5e5c0a
JezzaR <54776821+JezzaR-The-Proto@users.noreply.github.com>
2020-03-13 11:34:46 +0000

    Added more comments

--- e621 downloader.py
+++ e621 downloader.py
@@ -14,18 +14,18 @@ url += "&limit={0}&callback=callback".format(postnum) # Add rest of url
 req = requests.get(url) # Sends a get request to server to get back urls of images
 data = req.json() # Records the response as json
 data = str(data)
-res = [i for i in range(len(data)) if data.startswith("https://static1.e621.net/data", i)]
+res = [i for i in range(len(data)) if data.startswith("https://static1.e621.net/data", i)] # Find all strings starting with the e621 url
 print(res)
 print(url)
 x = 0
 while x < len(res):
     startpos = res[x]
-    endpos = res[x]+72
-    strings = data[startpos:endpos]
-    if "preview" in strings or "sample" in strings:
-        data = data [:startpos] + data[endpos+8:]
+    endpos = res[x]+72 # Standard url length
+    strings = data[startpos:endpos] # Set strings to whole url
+    if "preview" in strings or "sample" in strings: # Find if it is a low-res image
+        data = data [:startpos] + data[endpos+8:] # Remove the url
         res.pop(x)
     else:
         x += 1
-    res = [i for i in range(len(data)) if data.startswith("https://static1.e621.net/data", i)]
+    res = [i for i in range(len(data)) if data.startswith("https://static1.e621.net/data", i)] # Update res
 print(data)
